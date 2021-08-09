a9fff39b69b43cd95119f145ad9687899ddd4cae
Nilexis <almog1her@gmail.com>
2021-08-09 19:10:31 +0300

    fixed py code, added time stamps to the text file and added comments

--- main_lab.py
+++ main_lab.py
@@ -1,24 +1,41 @@
-import cv2;
-import pickle;
+import cv2
+import pickle
 import subprocess
 import time
 
-
-
-cap=cv2.VideoCapture(0)
-ret,frame=cap.read()
-retval=cv2.imwrite('frame1.png',frame)
+'''
+p = subprocess.Popen(['a.out'], shell=True, stdout=PIPE, stdin=PIPE)
+for ii in range(10):
+    value = str(ii) + '\n'
+    #value = bytes(value, 'UTF-8')  # Needed in Python 3.
+    p.stdin.write(value)
+    p.stdin.flush()
+    result = p.stdout.readline().strip()
+    print(result)
+'''
 
 frames_per_sec=15
-num_of_seconds=60
-fo = open("rgb.txt", "w+")
+num_of_seconds=10
+text_file_name= "rgb.txt"
+
+open(text_file_name, "w+").close() #clears the text file
 for i in range(num_of_seconds*frames_per_sec):
     
-    
+    #captures frame
     cap=cv2.VideoCapture(0)
-    ret,frame=cap.read()
-    retval=cv2.imwrite('frames/frame '+str(i)+'.png',frame)
+    ret,frame=cap.read() 
+    retval=cv2.imwrite('frames/'+str(i)+'.png',frame)
+    
+    #writes to the text file
+    list_of_frames = open(text_file_name, "a")
+    list_of_frames.write("frame: "+ str(i)+ ", time: " + str(time.time())+"\n")
+    list_of_frames.close()
+    
+    #waits for next frame
     time.sleep(1.0/frames_per_sec)
+    
+    
+    
     '''
     #subprocess
     args = ("bin/bar", "-c", "somefile.xml", "-d", "text.txt", "-r", "aString", "-f", "anotherString")
